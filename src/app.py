d922b2efaf70b832cf03e4bd86451beae04bc30d
brahi <b.munar@uniandes.edu.co>
2024-04-14 16:14:23 -0500

    CORS

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from src.controllers.ping_controller import Ping
 from src.controllers.exercise_controller import ExerciseController
 from src.controllers.training_plan_controller import TrainingPlanController
@@ -8,6 +9,7 @@ from src.controllers.training_plan_controller import TrainingPlanController
 def create_app():
     app = Flask(__name__)
     api = Api(app)
+    CORS(app)
     api.add_resource(Ping, '/training-plan/ping')
     api.add_resource(ExerciseController, '/training-plan/exercise')
     api.add_resource(TrainingPlanController, '/training-plan')
